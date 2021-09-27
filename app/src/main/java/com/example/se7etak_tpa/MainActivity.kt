da0505fe2f3e119336bc4b2db744cf780716d710
IslamAhmed1092 <eslam1092@hotmail.com>
2021-09-27 19:31:48 +0200

    add create account initial UI

--- app/src/main/java/com/example/se7etak_tpa/MainActivity.kt
+++ app/src/main/java/com/example/se7etak_tpa/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.se7etak_tpa
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            val intent = Intent(this, SignupActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
