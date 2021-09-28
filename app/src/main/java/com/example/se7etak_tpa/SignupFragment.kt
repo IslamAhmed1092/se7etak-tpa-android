7cf45ea90ea8f3ae385addf17294ba853e662288
IslamAhmed1092 <eslam1092@hotmail.com>
2021-09-28 14:43:38 +0200

    add data binding to project

--- app/src/main/java/com/example/se7etak_tpa/SignupFragment.kt
+++ app/src/main/java/com/example/se7etak_tpa/SignupFragment.kt
@@ -5,30 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.ImageButton
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.findNavController
+import com.example.se7etak_tpa.databinding.FragmentSignupBinding
 
 class SignupFragment : Fragment() {
+
+    private lateinit var binding: FragmentSignupBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_signup, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_signup, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val button = view.findViewById<Button>(R.id.btn_login)
-        button.setOnClickListener {
+        binding.btnLogin.setOnClickListener {
             val action = SignupFragmentDirections.actionSignupFragmentToLoginFragment()
             findNavController().navigate(action)
         }
 
-        val signupButton = view.findViewById<Button>(R.id.btn_signup)
-        signupButton.setOnClickListener {
+        binding.btnSignup.setOnClickListener {
             val action = SignupFragmentDirections.actionSignupFragmentToMobileVerificationFragment()
             findNavController().navigate(action)
         }
