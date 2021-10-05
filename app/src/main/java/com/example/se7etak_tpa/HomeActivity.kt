061c5daff85da32846954bf3c22344223b307717
IslamAhmed1092 <eslam1092@hotmail.com>
2021-10-05 20:22:14 +0200

    add analytics events in home page

--- app/src/main/java/com/example/se7etak_tpa/HomeActivity.kt
+++ app/src/main/java/com/example/se7etak_tpa/HomeActivity.kt
@@ -3,15 +3,31 @@ package com.example.se7etak_tpa
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.se7etak_tpa.databinding.ActivityHomeBinding
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.analytics.ktx.logEvent
+import com.google.firebase.ktx.Firebase
 
 class HomeActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityHomeBinding
+    private lateinit var navController: NavController
+    private lateinit var firebaseAnalytics: FirebaseAnalytics
+
+
+    private val listener = NavController.OnDestinationChangedListener { _, destination, _ ->
+        when(destination.id) {
+            R.id.navigation_request_approval -> firebaseAnalytics.logEvent("Request approval"){}
+            R.id.navigation_request_reimbursement -> firebaseAnalytics.logEvent("Request Re-imbursement"){}
+            R.id.navigation_check_network -> firebaseAnalytics.logEvent("Check Network"){}
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,13 +35,25 @@ class HomeActivity : AppCompatActivity() {
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        firebaseAnalytics = Firebase.analytics
         val navView: BottomNavigationView = binding.navView
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_home)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+        navController = findNavController(R.id.nav_host_fragment_activity_home)
 
         navView.setupWithNavController(navController)
 
+
     }
+
+
+    override fun onResume() {
+        super.onResume()
+        navController.addOnDestinationChangedListener(listener)
+    }
+
+    override fun onPause() {
+        navController.removeOnDestinationChangedListener(listener)
+        super.onPause()
+    }
+
 }
--- app/src/main/java/com/example/se7etak_tpa/home_ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/se7etak_tpa/home_ui/home/HomeFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
@@ -16,12 +17,17 @@ import com.example.se7etak_tpa.SignupViewModel
 import com.example.se7etak_tpa.data.User
 import com.example.se7etak_tpa.databinding.FragmentHomeBinding
 import com.example.se7etak_tpa.databinding.FragmentSignupBinding
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.analytics.ktx.logEvent
+import com.google.firebase.ktx.Firebase
 
 class HomeFragment : Fragment() {
 
     private val homeViewModel: HomeViewModel by viewModels()
     private lateinit var binding: FragmentHomeBinding
     private lateinit var user: User
+    private lateinit var firebaseAnalytics: FirebaseAnalytics
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -36,12 +42,17 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        firebaseAnalytics = Firebase.analytics
         binding.ivLogout.setOnClickListener {
             SignupViewModel.deleteUserData(requireContext())
             //TODO: send request to the server here
             val intent = Intent(context, AuthActivity::class.java)
             startActivity(intent)
             activity?.finish()
+        }
+        binding.ivProfile.setOnClickListener {
+            firebaseAnalytics.logEvent("Profile"){}
+            Toast.makeText(context, "Not implemented yet", Toast.LENGTH_SHORT).show()
         }
     }
 
